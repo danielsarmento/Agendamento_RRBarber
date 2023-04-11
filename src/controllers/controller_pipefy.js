8bfdceb8f816c75429bd6fc4ddbcc0eeea3f1287
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2023-04-11 15:14:23 -0300

    [FEAT]: Implementação do pipefy

---
+++ src/controllers/controller_pipefy.js
@@ -0,0 +1,43 @@
+function getFormattedDate() {
+    let date = new Date();
+    let day = date.getDate().toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear();
+    let hora = (date.getHours()-3).toString().padStart(2, '0')
+    let min = date.getMinutes().toString().padStart(2, '0')
+    let seg = date.getSeconds().toString().padStart(2, '0')
+    
+    return `${day}/${month}/${year}`;
+}
+
+export default async function createCard (req, res) {
+    const {phaseId, barbeiro, nomeCliente, horario, telefone, servico} = req.body
+    const pipeId = "303156913";
+    
+    try{
+        const novoCard = await fetch('https://api.pipefy.com/graphql',{
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json',
+                          'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDIzOTY0NjgsImVtYWlsIjoiZGFuaWVsc2FybWVudG8xNEBnbWFpbC5jb20iLCJhcHBsaWNhdGlvbiI6MzAwMjQyNzk2fX0.uBliz1cfgrfzOgzHHNmKsqRnJtnUyJCTdvtPl83ZUiOD3qN-Rp9mHihysZ97d8TNgL9N9nAWXDL_GisWJmbeSA"
+                        },
+                        body: JSON.stringify({
+                              "query": `mutation{ createCard (input: {pipe_id:${pipeId}  phase_id:${phaseId}  fields_attributes: [
+                                  {field_id: "barbeiro", field_value: "${barbeiro}"},
+                                  {field_id: "your_name", field_value: "${horario} hrs - ${getFormattedDate()} | ${nomeCliente}"},
+                                  {field_id: "telefone", field_value: "${telefone}"},
+                                  {field_id: "servi_o", field_value: "${servico}"}
+                                ]
+                                  }) 
+                                  { card {id title }}}`
+                        })
+        });
+        
+        const data = await novoCard.json();
+        return res.status(200).json(data)
+
+    } catch (err){
+        console.log(err)
+        res.status(500).end()
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,13 @@ const routes = express.Router()
 import controller_clientes from "../controllers/controller_clientes.js"
 import controller_barbeiro from "../controllers/controller_barbeiro.js"
 import {agendamentoCreate, agendamentoHorarios} from "../controllers/controller_agendamento.js"
+import controller_pipefy from "../controllers/controller_pipefy.js"
 
 routes.get("/", (req, res) => {res.json({mensagem: "API de Agendamento"})});
 routes.post("/cadastrar/cliente", controller_clientes);
 routes.post("/cadastrar/barbeiro", controller_barbeiro);
 routes.post("/cadastrar/agendamento", agendamentoCreate);
 routes.post("/buscar/horarios", agendamentoHorarios);
+routes.post("/criar/pipefy", controller_pipefy);
 
 export default routes;
